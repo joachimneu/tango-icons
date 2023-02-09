5be06f6834f19527699cb20bfcc7d9968916d33b
Joachim Neu <email@joachim-neu.de>
2023-02-09 15:02:47 -0800

    Icons from Wikimedia: https://commons.wikimedia.org/wiki/Tango_icons

--- src/tango-icons/generate.py
+++ src/tango-icons/generate.py
@@ -28,8 +28,10 @@ JINJA_SYNTAX = {
 script_folder = os.path.dirname(os.path.realpath(__file__))
 git_root_dir = os.path.abspath(os.path.join(script_folder, "..", ".."))
 source_folder = os.path.join(git_root_dir, "src")
-tangoicontheme_source_folder = os.path.join(source_folder, "tango-icon-theme")
-svg_source_folders = [ os.path.join(tangoicontheme_source_folder, "scalable") ]
+svg_source_folders = [
+    os.path.join(source_folder, "tango-icon-theme", "scalable"),
+    os.path.join(source_folder, "wiki-tango-icons", "publicdomain"),
+]
 target_folder = os.path.join(git_root_dir, "packages")
 pdf_temp_folder = os.path.join(git_root_dir, "packages", "pdf-tango-icons")
 all_tangoicons_pdf = os.path.join(git_root_dir, "packages", "all-tango-icons.pdf")
@@ -57,8 +59,14 @@ def convert_svg_to_pdf():
                 if filename[-4:] != ".svg":
                     continue
 
-                names.add(container + '_' + filename[:-4])
-                tar = f"{os.path.join(pdf_temp_folder, container + '_' + filename[:-4])}.pdf"
+                new_name = container + '_' + filename[:-4]
+                while new_name in names:
+                    new_name = new_name + '_'
+                # print(new_name)
+                assert not new_name in names
+                
+                names.add(new_name)
+                tar = f"{os.path.join(pdf_temp_folder, new_name)}.pdf"
 
                 inkscape_call.append(
                     f"file-open:{os.path.join(src_folder, filename)};"
---
+++ src/wiki-tango-icons/README.md
@@ -0,0 +1,5 @@
+The GNOME Tango icons (https://commons.wikimedia.org/wiki/GNOME_Desktop_icons) seem to all (?) be under CC BY-SA 3.0 US (the "Unofficial / Extras" ones at the end of the page seem to be under GPL), and were thus not included in this package.
+
+Other "non-official" icons from http://tango.freedesktop.org/Tango_Icon_Library were also not included.
+
+Unofficial Tango icons from https://commons.wikimedia.org/wiki/Tango_icons were downloaded and included, if their license was Public Domain or CC0.
